fd35739e63ca06c6fd0f2625fc42c8d126107cc6
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-12-19 12:59:53 +0530

    created route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch }  from 'react-router-dom';
+import { Route, Routes, Switch, Redirect } from "react-router-dom";
 import AllQuotes from './pages/NewQuote';
 import NewQuote from './pages/NewQuote';
 import QuoteDetail from './pages/QuoteDetail';
@@ -7,15 +7,20 @@ import QuoteDetail from './pages/QuoteDetail';
 function App() {
   return (
     <Switch>
-      <Route path="/quotes">
-        <AllQuotes/>
-      </Route>
-      <Route path="/quotes/:quoteId">
-        <QuoteDetail/>
-      </Route>
-      <Route path="/new-quote">
-        <NewQuote/>
-      </Route>
+      <Routes>
+        <Route path="/quotes" exact>
+          <Redirect to="/quotes"/>
+        </Route>
+        <Route path="/quotes" exact>
+          <AllQuotes />
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetail />
+        </Route>
+        <Route path="/new-quote">
+          <NewQuote />
+        </Route>
+      </Routes>
     </Switch>
   );
   };
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,6 +1,17 @@
-const QuoteDetail  = () => {
+import { useParams } from 'react';
+import { Fragment, Route } from 'react';
 
-    return<h1> Quote Detail page </h1>
+const QuoteDetail  = () => {
+const params = useParams();
+    
+return (
+  <Fragment>
+
+    <h1> Quote Detail page </h1>
+    <p>{params.quoteId}</p>
+
+  </Fragment>
+);
 };
 
 export default  QuoteDetail;
