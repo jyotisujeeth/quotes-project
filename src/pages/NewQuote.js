25af803e63410fdd0ba621de833c924297642611
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-12-21 01:38:51 +0530

    use history

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,7 +1,20 @@
+import { useHistory } from 'react-route-dom';
+
 
 
 const AllQuotes = () => {
- return<h1> New....Quote Detail page </h1>
+const history = useHistory();
+
+const addQuotesHandler = {quoteData} => {
+    console.log('addQuotesHandler');
+    history.push('/quote');
+};
+
+
+ return(
+ <h1> New....Quote Detail page </h1>
+<QuoteFrom onAddQuote= {addQuotehandler}></QuoteFrom>
+ );
 };
 
 
